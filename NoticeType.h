95f42182aa64795c3dd747445c8145523078aeda
Thakasa Ngcobo <u25556526@tuks.co.za>
2026-08-29 22:40:40 +0200

    Added Observer and Subject interface headers

---
+++ NoticeType.h
@@ -0,0 +1,18 @@
+#ifndef NOTICETYPE_H
+#define NOTICETYPE_H
+
+/**
+ * @brief Event-wide notice categories issued by EventControl.
+ */
+enum class NoticeType {
+    OPEN,
+    CLOSE,
+    SCHEDULE_CHANGE,
+    CAPACITY_ALERT,
+    WEATHER_ALERT,
+    PAUSE,
+    RESUME,
+    EVACUATE
+};
+
+#endif // NOTICETYPE_H
---
+++ Observer.h
@@ -0,0 +1,23 @@
+#ifndef OBSERVER_H
+#define OBSERVER_H
+
+class Notice;
+
+/**
+ * @brief Abstract Observer interface for receiving Notices.
+ */
+class Observer {
+public:
+    /**
+     * @brief Pushes a Notice to this Observer.
+     * @param notice The delivered Notice. Reference is only valid during the call.
+     */
+    virtual void update(const Notice& notice) = 0;
+
+    /**
+     * @brief Virtual destructor.
+     */
+    virtual ~Observer() = default;
+};
+
+#endif // OBSERVER_H
